df73b44155e937086f55fdca19cef5777d7e729d
Pibo Shongwe <piboman@yahoo.com>
2023-07-08 14:21:04 +0200

    Create BinarySearch.js

---
+++ JavaScript/BinarySearch.js
@@ -0,0 +1,20 @@
+function binarySearch(searchList, value) {
+  let arrayPath = [];
+  let startIndex = 0;
+  let endIndex = searchList.length - 1;
+
+  while (startIndex <= endIndex) {
+    let middleIndex = Math.floor((startIndex + endIndex) / 2);
+    arrayPath.push(middleIndex);
+
+    if (searchList[middleIndex] === value) {
+      return arrayPath;
+    } else if (searchList[middleIndex] < value) {
+      startIndex = middleIndex + 1;
+    } else {
+      endIndex = middleIndex - 1;
+    }
+  }
+
+  return arrayPath;
+}
